53836a05e74cd449e5fbdd913b35c6148c1e7ae3
drkrieger1 <eplyushko@gmail.com>
2017-12-04 16:54:01 -0800

    react added to project and start up class

--- ReactComments/ReactComments/Startup.cs
+++ ReactComments/ReactComments/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using React.AspNet;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ReactComments
@@ -15,6 +16,12 @@ namespace ReactComments
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            //React Services Dependency Injection 
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact(); 
+
+
             services.AddMvc();
         }
 
@@ -25,7 +32,24 @@ namespace ReactComments
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                // .AddScript("~/Scripts/First.jsx")
+                // .AddScript("~/Scripts/Second.jsx");
 
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+            });
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
 
