3d6ed41f792423348df5e326cd5e11ac4f649eec
drkrieger1 <eplyushko@gmail.com>
2017-12-05 12:45:40 -0800

    JSX file wont work in wwwRoot

--- ReactComments/ReactComments/wwwroot/js/tutorial.js
+++ ReactComments/ReactComments/wwwroot/js/tutorial.js
@@ -1,34 +1,11 @@
 ﻿
-////Commment box
-//var CommentBox = React.createClass({
-//    render: function () {
-//        return (
-//            <div className="commentBox">
-//                Hello, World! I am a CommentBox. 
-//            </div>
-//          );
-//    }
-//});
-//ReactDOM.render(
-//    <CommentBox />,
-//    document.getElementById('content')
-//);
-
-
-//var CommentBox = React.createClass({
-//    render: function () {
-//        return (
-//            <div className="commentBox">
-//                Hello, world! I am a CommentBox.
-//      </div>
-//        );
-//    }
-//});
-//ReactDOM.render(
-//    <CommentBox />,
-//    document.getElementById('content')
-//);
-
+// Commment box
+// Not sure why, but this block of JSX syntax code only works with .JS file types. 
+// wwwRoot does not recognioce .JSX at all, and if you use js syntax in the.JS it wont render the react code. 
+// tutorial.jsx + React code with js syntax = no render and jsx file type is not seen by wwwRoot.
+// tutorial.jsx + React code with jsx syntax = no render and jsx file type is not seen by wwwRoot.
+// tutorial.js + React code with js syntax = no render but js file type works with wwwRoot.
+// tutorial.js + React code with jsx syntax = Render. 
 var CommentBox = React.createClass({
     displayName: 'CommentBox',
     render: function () {
@@ -43,3 +20,17 @@ ReactDOM.render(
     React.createElement(CommentBox, null),
     document.getElementById('content')
 );
+
+//var CommentBox = React.createClass({
+//    render: function () {
+//        return (
+//            <div className="commentBox">
+//                Hello, world! I am a CommentBox.
+//      </div>
+//        );
+//    }
+//});
+//ReactDOM.render(
+//    <CommentBox />,
+//    document.getElementById('content')
+//);
