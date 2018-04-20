28994b6a4aeff4421e566c0477ac85b9b71759c1
drkrieger1 <eplyushko@gmail.com>
2018-04-19 17:45:29 -0700

    ng derective

--- angularapp/src/app/components/users/users.component.ts
+++ angularapp/src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
   constructor() { }
 
   ngOnInit() {
+    this.users = [
+      {
+        firstName: 'Erik',
+        lastName: 'Plyushko',
+        age: 26,
+        address: {
+            street: '1234 5th ave',
+            city: 'Seattle',
+            zip: 98002
+        }
+     },
+     {
+        firstName: 'Bill',
+        lastName: 'Johnsen',
+        age: 20,
+        address: {
+          street: '1354 10th ste',
+          city: 'Seattle',
+          zip: 98055
+      }
+     },
+     {
+        firstName: 'Jill',
+        lastName: 'Basher',
+        age: 30,
+        address: {
+          street: '1436 2nd ave',
+          city: 'Seattle',
+          zip: 98002
+      }
+     }
+    ];
+
+    this.addUser({
+      firstName: 'Kirill',
+      lastName: 'Plyushko',
+      age: 25,
+      address: {
+        street: '55 mill st',
+        city: 'Miami',
+        zip: 98198
+      }
+    });
+  }
+
+  addUser(user:User) {
+    this.users.push(user);
   }
 
 }
