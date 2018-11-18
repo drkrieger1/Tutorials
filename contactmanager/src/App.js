f97ec7c0edaeb3195883917e04a4efe02811264d
drkrieger1 <eplyushko@gmail.com>
2018-11-18 14:37:04 -0800

    class components and functional components

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App component</h1>
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@mail.com" phone="555-555-555" />
+        <Contact name="jane Doe" email="janedoe@mail.com" phone="555-777-888" />
       </div>
     );
   }
