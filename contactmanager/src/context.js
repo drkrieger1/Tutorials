f5e3968ee166f767d0f343a285210dbf8336fedf
Erik Plyushko <eplyushko@gmail.com>
2018-11-25 17:32:23 -0800

    React tutorial  Part (#1)
    * class components and functional components
    
    * typechecking
    
    * bootstrap install & setup
    
    * working with state
    
    * refactor to use context and reducer

---
+++ contactmanager/src/context.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Bil Smith',
+        email: 'bsmith@mail.com',
+        phone: '321-343-8756'
+      },
+      {
+        id: 2,
+        name: 'Anne Jones',
+        email: 'ajones@mail.com',
+        phone: '321-343-8756'
+      },
+      {
+        id: 3,
+        name: 'Randy Hill',
+        email: 'rhill@mail.com',
+        phone: '123-433-7788'
+      }
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    }
+  };
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
