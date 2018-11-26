a4fc4393b24b3808af69c3be896c7d9217c8ffd4
drkrieger1 <eplyushko@gmail.com>
2018-11-25 17:29:33 -0800

    refactor to use context and reducer

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../context';
 
 class Contact extends Component {
   constructor(props) {
@@ -13,37 +14,49 @@ class Contact extends Component {
     this.setState({ showInfo: !this.state.showInfo });
   }
 
-  onDeleteClick() {
-    this.props.deleteHandler();
+  onDeleteClick(id, dispatch) {
+    console.log('in delete', id);
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
   }
 
   render() {
     const { showInfo } = this.state;
 
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          <i
-            onClick={this.handleClick.bind(this)}
-            className={
-              showInfo ? 'fas fa-sort-up mr-2' : 'fas fa-sort-down mr-2'
-            }
-            style={{ cursor: 'pointer' }}
-          />
-          {this.props.name}
-          <i
-            onClick={this.onDeleteClick.bind(this)}
-            className="fas fa-times"
-            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
-          />
-        </h4>
-        {showInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {this.props.email}</li>
-            <li className="list-group-item">Phone: {this.props.phone}</li>
-          </ul>
-        ) : null}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                <i
+                  onClick={this.handleClick.bind(this)}
+                  className={
+                    showInfo ? 'fas fa-sort-up mr-2' : 'fas fa-sort-down mr-2'
+                  }
+                  style={{ cursor: 'pointer' }}
+                />
+                {this.props.name}
+                <i
+                  className="fas fa-times"
+                  style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                  onClick={this.onDeleteClick.bind(
+                    this,
+                    this.props.id,
+                    dispatch
+                  )}
+                />
+              </h4>
+              {showInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {this.props.email}</li>
+                  <li className="list-group-item">Phone: {this.props.phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
