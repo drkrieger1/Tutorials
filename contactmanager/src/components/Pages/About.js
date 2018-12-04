f64fd4434d1a3c1ed0b989eab4da2527766aaa40
drkrieger1 <eplyushko@gmail.com>
2018-12-03 16:58:33 -0800

    about ipsum

--- contactmanager/src/components/Pages/About.js
+++ contactmanager/src/components/Pages/About.js
@@ -4,7 +4,15 @@ export default function About() {
   return (
     <div>
       <h1 className="display-4">About Contact Manager</h1>
-      <p>Simple app to manage contacts</p>
+      <p>
+        PBR&B schlitz authentic tattooed flexitarian, affogato ethical.
+        Succulents narwhal thundercats butcher seitan tofu paleo meditation
+        neutra offal man bun. Unicorn pok pok air plant, typewriter offal cred
+        brooklyn iPhone thundercats distillery poutine hoodie 3 wolf moon craft
+        beer raclette. Beard cronut hoodie, edison bulb etsy williamsburg four
+        dollar toast kickstarter vape hot chicken craft beer chicharrones kogi
+        selfies.
+      </p>
     </div>
   );
 }
